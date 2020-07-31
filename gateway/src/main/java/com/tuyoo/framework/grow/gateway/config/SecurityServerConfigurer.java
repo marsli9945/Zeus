d6d68a202a3457070d05020fa5f10e8b97512381
lilei <li8369945>
2020-07-31 17:51:59 +0800

    过滤文件上传地址

--- gateway/src/main/java/com/tuyoo/framework/grow/gateway/config/SecurityServerConfigurer.java
+++ gateway/src/main/java/com/tuyoo/framework/grow/gateway/config/SecurityServerConfigurer.java
@@ -48,7 +48,10 @@ public class SecurityServerConfigurer
                 .pathMatchers("/v1/auth/io200NoAuth").permitAll()
                 // admin服务下ga系统登陆注册相关接口允许匿名访问
                 .pathMatchers("/v1/admin/ga/system/**").permitAll()
+                // 资源上传的接口允许匿名访问
                 .pathMatchers("/v1/admin/game/upload").permitAll()
+                .pathMatchers("/v1/insight/file/upload").permitAll()
+                .pathMatchers("/v1//insight/group-service/upload").permitAll()
                 // 系统健康检查和对外监控允许匿名
                 .pathMatchers("/actuator/**").permitAll()
                 .pathMatchers("/monitor").permitAll()
